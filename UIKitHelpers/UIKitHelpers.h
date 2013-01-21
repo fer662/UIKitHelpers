94cf43e472b410dac5308297ad24dcb7b33f08e7
Fernando Mazzon <Fernando.Mazzon@rga.com>
2013-01-21 13:34:35 -0300

    no message

---
+++ UIKitHelpers/UIKitHelpers.h
@@ -0,0 +1,13 @@
+//
+//  UIKitHelpers.h
+//  UIKitHelpers
+//
+//  Created by Fernando Mazzon on 1/21/13.
+//  Copyright (c) 2013 Fernando Mazzon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIKitHelpers : NSObject
+
+@end
