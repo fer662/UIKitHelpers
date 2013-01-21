328b2a99013b21f40e503aa8a0554b774bd51782
Fernando Mazzon <Fernando.Mazzon@rga.com>
2013-01-21 14:08:54 -0300

    namespace

--- Classes/UIView+FrameHelpers.h
+++ Classes/UIView+FrameHelpers.h
@@ -10,18 +10,18 @@
 
 @interface UIView (FrameHelpers)
 
-@property (nonatomic) CGFloat frameX_oui;
-@property (nonatomic) CGFloat frameY_oui;
-@property (nonatomic) CGFloat frameWidth_oui;
-@property (nonatomic) CGFloat frameHeight_oui;
+@property (nonatomic) CGFloat frameX_rga;
+@property (nonatomic) CGFloat frameY_rga;
+@property (nonatomic) CGFloat frameWidth_rga;
+@property (nonatomic) CGFloat frameHeight_rga;
 
-@property (nonatomic) CGSize frameSize_oui;
-@property (nonatomic) CGPoint frameOrigin_oui;
+@property (nonatomic) CGSize frameSize_rga;
+@property (nonatomic) CGPoint frameOrigin_rga;
 
-@property (nonatomic) CGFloat centerX_oui;
-@property (nonatomic) CGFloat centerY_oui;
+@property (nonatomic) CGFloat centerX_rga;
+@property (nonatomic) CGFloat centerY_rga;
 
-@property (nonatomic, readonly) CGFloat frameMaxX_oui;
-@property (nonatomic, readonly) CGFloat frameMaxY_oui;
+@property (nonatomic, readonly) CGFloat frameMaxX_rga;
+@property (nonatomic, readonly) CGFloat frameMaxY_rga;
 
 @end
--- Classes/UIView+FrameHelpers.m
+++ Classes/UIView+FrameHelpers.m
@@ -10,67 +10,104 @@
 
 @implementation UIView (FrameHelpers)
 
-- (CGFloat)frameX_oui        { return self.frame.origin.x; }
-- (CGFloat)frameY_oui        { return self.frame.origin.y; }
-- (CGFloat)frameWidth_oui    { return self.frame.size.width; }
-- (CGFloat)frameHeight_oui   { return self.frame.size.height; }
-- (CGSize)frameSize_oui      { return self.frame.size; }
-- (CGPoint)frameOrigin_oui   { return self.frame.origin; }
+- (CGFloat)frameX_rga
+{
+    return self.frame.origin.x;
+}
 
--(void)setFrameX_oui:(CGFloat)x
+- (CGFloat)frameY_rga
+{
+    return self.frame.origin.y;
+}
+
+- (CGFloat)frameWidth_rga
+{
+    return self.frame.size.width;
+}
+
+- (CGFloat)frameHeight_rga
+{
+    return self.frame.size.height;
+}
+
+- (CGSize)frameSize_rga
+{
+    return self.frame.size;
+}
+
+- (CGPoint)frameOrigin_rga
+{
+    return self.frame.origin;
+}
+
+- (void)setFrameX_rga:(CGFloat)x
 {
     CGRect rect = self.frame;
     rect.origin.x = x;
     self.frame = rect;
 }
 
--(void)setFrameY_oui:(CGFloat)y
+- (void)setFrameY_rga:(CGFloat)y
 {
     CGRect rect = self.frame;
     rect.origin.y = y;
     self.frame = rect;
 }
 
--(void)setFrameWidth_oui:(CGFloat)width
+- (void)setFrameWidth_rga:(CGFloat)width
 {
     CGRect rect = self.frame;
     rect.size.width = width;
     self.frame = rect;
 }
 
--(void)setFrameHeight_oui:(CGFloat)height
+- (void)setFrameHeight_rga:(CGFloat)height
 {
     CGRect rect = self.frame;
     rect.size.height = height;
     self.frame = rect;
 }
 
--(void)setFrameSize_oui:(CGSize)size
+- (void)setFrameSize_rga:(CGSize)size
 {
     CGRect rect = self.frame;
     rect.size = size;
     self.frame = rect;
 }
 
--(void)setFrameOrigin_oui:(CGPoint)origin
+- (void)setFrameOrigin_rga:(CGPoint)origin
 {
     CGRect rect = self.frame;
     rect.origin = origin;
     self.frame = rect;
 }
 
--(CGFloat)frameMaxX_oui     { return CGRectGetMaxX(self.frame); }
--(CGFloat)frameMaxY_oui     { return CGRectGetMaxY(self.frame); }
+- (CGFloat)frameMaxX_rga
+{
+    return CGRectGetMaxX(self.frame);
+}
 
--(CGFloat)centerX_oui   { return self.center.x; }
--(CGFloat)centerY_oui   { return self.center.y; }
+- (CGFloat)frameMaxY_rga
+{
+    return CGRectGetMaxY(self.frame);
+}
 
--(void)setCenterX_oui:(CGFloat)centerX
+- (CGFloat)centerX_rga
+{
+    return self.center.x;
+}
+
+- (CGFloat)centerY_rga
+{
+    return self.center.y;
+}
+
+- (void)setCenterX_rga:(CGFloat)centerX
 {
     self.center = CGPointMake(centerX, self.center.y);
 }
 
--(void)setCenterY_oui:(CGFloat)centerY
+- (void)setCenterY_rga:(CGFloat)centerY
 {
     self.center = CGPointMake(self.center.x, centerY);
 }
